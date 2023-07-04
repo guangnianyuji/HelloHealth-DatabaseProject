153da7d93d5b97cf51de9897c5806d24b75880be
guangnianyuji <2152085@tongji.edu.cn>
2023-07-04 16:21:46 +0800

    demo login

---
+++ front-end/src/main.js
@@ -0,0 +1,17 @@
+import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import "@/assets/main.css";
+import '@/assets/misans.css'
+
+
+import App from './App.vue'
+import router from './router'
+// import './mock'
+
+const app = createApp(App)
+app.use(ElementPlus)
+
+app.use(router)
+
+app.mount('#app')
