9cbdfa7eed41817fb8ed32749b58b403f09bf8aa
hugeBlack <wwkq03@outlook.com>
2023-07-04 15:13:11 +0800

    front init

---
+++ src/components/HomeContainer.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="headerHolder">
+        <img alt="" src="@/assets/logo.png">
+    </div>
+    <div class="contentHolder">
+        <div class="sideBar">
+
+        </div>
+        <div class="content">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
