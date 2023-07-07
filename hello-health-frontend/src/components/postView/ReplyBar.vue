16f6ccaac9d7ea79ec9d288654b96d11e3a52063
hugeBlack <wwkq03@outlook.com>
2023-07-07 16:26:53 +0800

    帖子页面差不多写完了，给药品界面加了路由，修复了bug，加了global

---
+++ hello-health-frontend/src/components/postView/ReplyBar.vue
@@ -0,0 +1,39 @@
+<script setup>
+import {ref} from "vue";
+import globalData from "@/global/global"
+const emit = defineEmits(["replySubmit"])
+const replyContent = ref('')
+const submitReply = () => {
+    emit("replySubmit",replyContent)
+}
+</script>
+
+<template>
+    <div class="replyBar">
+        <el-avatar class="avatar" :src="globalData.userInfo.avatar_url" :size="32"></el-avatar>
+        <el-input class="input" v-model="replyContent" type="textarea" :rows="1"></el-input>
+        <el-button class="btn" @click="submitReply" type="primary">回复</el-button>
+    </div>
+</template>
+
+<style scoped>
+.replyBar{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin: 20px;
+}
+
+.input{
+    margin: 0 10px;
+}
+
+.avatar{
+    height: 32px;
+    min-height: 32px;
+    width: 32px;
+    min-width: 32px;
+}
+
+</style>
--- hello-health-frontend/src/views/PostView.vue
+++ hello-health-frontend/src/views/PostView.vue
@@ -1,8 +1,9 @@
 <script setup>
 import PostFloor from "@/components/postView/PostFloor.vue";
-import {computed, reactive} from "vue";
+import {computed, reactive, ref} from "vue";
 import axios from "axios";
 import {useRoute} from "vue-router";
+import globalData from "@/global/global";
 const router = useRoute()
 
 let postId = router.params.postId;
@@ -15,6 +16,14 @@ const floorsWithoutFirst = computed(() =>{
     return postInfo.data.floors?.slice(1)
 })
 
+const floors = ref()
+
+const dialogVisible = ref(false)
+
+const openCommentEditor = () =>{
+    dialogVisible.value = true
+}
+
 axios.get("/api/PostInfo/"+ postId)
 .then((res) => {
         let responseObj = res.data;
@@ -22,15 +31,24 @@ axios.get("/api/PostInfo/"+ postId)
             alert("错误代码："+ responseObj.errorCode);
             return;
         }
-        if(!responseObj.data.status){
-            alert("帖子加载失败："+ responseObj.data.errorType);
-            return;
-        }
+        // if(!responseObj.data.status){
+        //     alert("帖子加载失败："+ responseObj.data.errorType);
+        //     return;
+        // }
         postInfo.data = responseObj.data
     }
 ).catch((reason)=>{
     alert(reason)
 })
+
+
+const closeAllFloorReplyBar = () =>{
+    console.log(floors.value)
+    for(let floor of floors.value){
+        floor.closeAllReplyBar()
+    }
+}
+
 </script>
 
 <template>
@@ -40,14 +58,53 @@ axios.get("/api/PostInfo/"+ postId)
                     :title="postInfo.data.title"
                     :is-bounty="postInfo.data.is_bounty"
                     :bounty-value="postInfo.data.bounty_value"
-                    :solution="postInfo.data.solution"></post-floor>
-        <post-floor v-for="floor in floorsWithoutFirst" :floor-info="floor"></post-floor>
+                    :solution="postInfo.data.solution" @replyClicked="closeAllFloorReplyBar"></post-floor>
+        <post-floor v-for="(floor,index) in floorsWithoutFirst" :floor-info="floor" ref="floors" @replyClicked="closeAllFloorReplyBar"></post-floor>
     </div>
+    <div class="writeFloorButton" @click="openCommentEditor" v-if="globalData.login">
+        <i class="fi fi-sr-feather"></i>
+    </div>
+    <el-dialog
+        v-model="dialogVisible"
+        title="Tips"
+        width="30%"
+    >
+        <span>This is a message</span>
+        <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible.value = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogVisible = false">
+          Confirm
+        </el-button>
+      </span>
+        </template>
+    </el-dialog>
 </template>
 
 <style scoped>
 .viewWrapper{
+    position: relative;
     width: 85%;
     margin: 0 auto;
+}
+
+.writeFloorButton{
+    position: absolute;
+    right: 7.5%;
+    bottom: 100px;
+    height: 70px;
+    width: 70px;
+    border-radius: 50%;
+    background-color: var(--el-color-primary);
+    color: #fff;
+    font-size: 25px;
+    box-shadow: 0 6px 15px rgba(36,37,38,.2);
+    text-align: center;
+    line-height: 75px;
+    cursor: pointer;
+}
+
+.writeFloorButton:hover{
+    background-color: var(--el-color-primary-light-2);
 }
 </style>
