a1f1e4446243ac897dcbfe9bf91d4f9ce82c3b66
hugeBlack <wwkq03@outlook.com>
2023-07-08 17:50:47 +0800

    改了些样式，添加了论坛的路由

--- hello-health-frontend/src/components/LikeButton.vue
+++ hello-health-frontend/src/components/LikeButton.vue
@@ -1,22 +1,19 @@
 <template>
     <div>
     <span
-      style="text-align: left; margin-right: 8px"
-      v-if="!is_liked">
+        style="text-align: left; margin-right: 8px"
+        v-if="!is_liked">
         <i class="fi fi-rr-social-network" @click="like"></i>
     </span>
-    <span
-      style="text-align: left; margin-right: 8px"
-      v-else
-    >
+        <span
+            style="text-align: left; margin-right: 8px"
+            v-else
+        >
         <i class="fi fi-sr-thumbs-up" @click="like"></i>
         
     </span>
-   
-    <span
-     style="like-number">
-        {{like_num}}
-     </span>
+
+        <span>{{like_num}}</span>
 
     </div>
 
@@ -34,76 +31,56 @@
 import { ElMessage } from 'element-plus'
 import { reactive } from 'vue';
 import axios from "axios";
-    export default
-    {
-        props:["comment_id"],
-        data:()=>
+export default
+{
+    props:["comment_id"],
+    data:()=>
         ({
             is_liked: false,
             like_num: 0,
-            comment_id: 0,
-
-            //云端mock地址，可删
-            test_add: "https://mock.apifox.cn/m1/2961538-0-default"
         }),
-        watch:
-        {
-            comment_id:function(newData)
-             {
-                this.comment_id=newData;
-             }
-        },
-        methods:
+    methods:
         {
             like()
             {
                 console.log("cl");
-            
+
                 this.changeLike(1);
             },
-            changeLike(op)
-            {//op为0，只查询；op为1 要操作
-                            
-            //以后全局获取user_id,没有登录就是-1
-            //此处要检测有没有登录
-            //没有的话要跳转去登录
-                let user_id=-1;
+            changeLike(op) {
+                //op为0，只查询；op为1 要操作
 
-          
-                axios.post(this.test_add+"/api/Comment/Like",
-                    reactive({
-                       operate:op,
-                       user_id:user_id,
-                       comment_id: this.comment_id
-                    }))
-                    .then((res)=>{
-                        this.is_liked=res.data.data.status;
-                         this.like_num=res.data.data.comment_like_num;
-                    }) ;
-                
-           
- 
-                if(op==1)//操作//只会在登录情况下走进下面的语句
+                //此处要检测有没有登录
+                //没有的话要跳转去登录
+                axios.post("/api/Comment/Like", {
+                    operate:op,
+                    comment_id:this.comment_id,
+                }).then((res)=>{
+                    this.is_liked=res.data.data.status;
+                    this.like_num=res.data.data.comment_like_num;
+                });
+
+                if(op===1)//操作//只会在登录情况下走进下面的语句
                 {
-                  let message="";
-                  if(this.is_liked==true)
-                 {
-                         message="点赞成功！"
-                 }
-                 else
-                {
-                       message="取消点赞成功！"
-                }
+                    let message="";
+                    if(this.is_liked===true)
+                    {
+                        message="点赞成功！"
+                    }
+                    else
+                    {
+                        message="取消点赞成功！"
+                    }
                     ElMessage({
                         showClose: true,
                         message:message,
                         type: 'success',
-                     })
+                    })
                 }
             }
         },
-        created(){
-            this.changeLike(0);
-        }
+    created(){
+        this.changeLike(0);
     }
+}
 </script>
--- hello-health-frontend/src/components/PostCard.vue
+++ hello-health-frontend/src/components/PostCard.vue
@@ -1,87 +1,85 @@
 <template>
-   <el-card :body-style="{ padding: '0px'}"
-   shadow="never">
-    <img  
-        :src="post_info.post_pic"
-        class="card_image"
-         
-        />
-     <!-- 
-    <img v-else
-        src="../assets/loading.gif"
-        class="image" 
-        />
-      -->
-    <!--显示标签 -->
-    <div style="padding: 14px">
-        <el-row
-          gutter="10"
-          justify="left"
-        >
-          <el-col
-            span="1"
-            v-for="tag in post_info.post_tag"
-            :key="tag"
-          >
-            <el-tag
-              class="ml-2"
-              type="success"
-              size="small"
-            >{{ tag }}</el-tag>
-          </el-col>
-        </el-row>
-    </div>
-    <!--显示帖子名称-->
-      <div class="card_title  click_title" >
-        <span @click="goDetail"  >{{PostTitleSummary(post_info.post_title)}}</span>
-      </div>
-    <!--显示发帖人-->
-      <section class="card_author">
-        <el-row
-          align="middle"
-          style="margin-left:5%;"
-        >
-        <el-col :span="2">
-        <el-avatar :size="25" :src="post_info.author_portrait" ></el-avatar>
-
-        </el-col>
-        <el-col :span="8" style="text-align: left"> 
-        <span class="card_author">
+    <el-card class="card-wrapper"
+             shadow="never">
+        <img alt="" :src="post_info.post_pic" class="card_image"/>
+        <!--
+       <img v-else
+           src="../assets/loading.gif"
+           class="image"
+           />
+         -->
+        <!--显示标签 -->
+        <div style="padding: 14px">
+            <div class="post-tag-bar">
+                    <el-tag type="success" size="small" v-for="tag in post_info.post_tag">{{ tag }}</el-tag>
+            </div>
+        </div>
+        <!--显示帖子名称-->
+        <div class="card_title click_title" >
+            <span @click="goDetail"  >{{PostTitleSummary(post_info.post_title)}}</span>
+        </div>
+        <!--显示发帖人-->
+        <section class="card_author">
+            <el-row align="middle" style="margin-left:5%;">
+                <el-col :span="10">
+                    <el-avatar :size="25" :src="post_info.author_portrait" ></el-avatar>
+                    <span class="card_author">
           {{ post_info.author_name }}
         </span>
-        </el-col>
-        <el-col :span="6">
-            <like-button :comment_id="post_first_comment_id" />
-        </el-col>
-        <el-col :span="6">
-            <coin-button :comment_id="post_first_comment_id" />
-        </el-col>
-        </el-row>
-      </section>
-    
-  </el-card>
-    
+                </el-col>
+                <el-col :span="6">
+                    <like-button :comment_id="post_info.post_id" />
+                </el-col>
+                <el-col :span="6">
+                    <coin-button :comment_id="post_info.post_id" />
+                </el-col>
+            </el-row>
+        </section>
+
+    </el-card>
+
 </template>
 
 <style>
 .card_image {
-  object-fit: cover;
-  height: 100%;
-  width: 100%;
+    object-fit: cover;
+    height: 100%;
+    width: 100%;
 }
 .card_title {
-  font-size: x-large;
-  text-align: center;
-  margin-top: 15px;
-  margin-bottom: 15px;
-}
-.click_title:hover{
-  text-decoration: underline;
+    font-size: x-large;
+    text-align: center;
+    margin-top: 15px;
+    margin-bottom: 15px;
 }
 .card_author{
     display:inline;
     text-align:center;
     font-size: medium;
+}
+.post-tag-bar{
+    width: 100%;
+    flex-wrap: wrap;
+    display: flex;
+    flex-direction: row;
+    margin: -3px -3px;
+}
+.post-tag-bar>*{
+    margin: 3px;
+}
+
+.card-wrapper .el-card__body{
+    padding: 0 !important;
+    margin-bottom: 10px;
+}
+
+.card-wrapper{
+    transition: 0.3s ease;
+}
+
+.card-wrapper:hover{
+    box-shadow: 0 6px 6px rgba(36,37,38,.1);
+    transform: translateY(-2px);
 }
 
 </style>
@@ -90,50 +88,47 @@
 import LikeButton from "./LikeButton.vue";
 import CoinButton from "../components/CoinButton.vue";
 import { ElMessage } from "element-plus";
-    export default
-    {
-        components: 
+import router from "@/router";
+export default
+{
+    components:
         {
             LikeButton,
             CoinButton,
-            
             ElMessage,
         },
-        props: ["post_info"],
-        watch:
+    props: ["post_info"],
+    watch:
         {
             post_info:function(newData)
-             {
+            {
                 this.comment_id=newData.post_first_comment_id;
-             }
+            }
         },
-        data:()=>
+    data:()=>
         ({
-              comment_id:0,//这个帖子的一楼评论，帖子内容本身
+            comment_id:0,//这个帖子的一楼评论，帖子内容本身
+            post_id:0,
         }),
-        methods:
+    methods:
         {
-             goDetail () {
-                this.$router.push({
-                path: "/blog_detail",
-                query: {
-                  post_id: this.post_info.post_id,
-
-                 },
-              });
+            goDetail () {
+                router.push({
+                    path: "/forum/"+this.post_info.post_id,
+                });
             },
             //防止标题过长
             PostTitleSummary(title){
-              if(title.length<10){
-                return title;
-              }
-              else{
-                return title.slice(0,12)+"...";
-              }
+                if(title.length<10){
+                    return title;
+                }
+                else{
+                    return title.slice(0,12)+"...";
+                }
             }
         },
-        created(){
-            
-        }
+    created(){
+
     }
+}
 </script>
