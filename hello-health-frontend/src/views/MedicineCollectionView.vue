ee62bb973326ec7c96ae59c01de1b9ff430f545b
Huge_Black <wwkq03@outlook.com>
2023-07-18 00:24:44 +0800

    去掉了多余的WebLoading

--- hello-health-frontend/src/views/MedicineCollectionView.vue
+++ hello-health-frontend/src/views/MedicineCollectionView.vue
@@ -5,8 +5,6 @@
 
 <template>
     <div class="Medicine-Collection">
-      <BackButton />
-      <WebLoading />
       <div class="FM_body">
         <el-row class="FM_title">
           <svg t="1688614393322" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
