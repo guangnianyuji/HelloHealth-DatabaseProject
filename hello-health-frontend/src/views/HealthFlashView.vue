4d5fb9ba747071d84143713a33e5b01fcc93a4c1
Karry <125780538+Iuriak@users.noreply.github.com>
2023-09-07 11:14:28 +0800

    健康资讯美化和动效制作

--- hello-health-frontend/src/views/HealthFlashView.vue
+++ hello-health-frontend/src/views/HealthFlashView.vue
@@ -5,7 +5,7 @@
 <template>
   <div class="HF_body">
     <!-- 新闻轮播器 -->
-    <NewsCarousel/>
+    <NewsCarousel />
     <el-row class="HF_title">
       <div class="viewTitle tracking-in-expand" style="padding-top: 3px">健康资讯</div>
     </el-row>
@@ -29,11 +29,11 @@
 import NewsCarousel from "../components/NewsCarousel.vue";
 import NewsBlockList from "../components/NewsBlockList.vue";
 import NewsTagSelector from "../components/NewsTagSelector.vue";
-import {Search} from "@element-plus/icons-vue";
+import { Search } from "@element-plus/icons-vue";
 
 export default {
   name: "HealthFlashView",
-  components: {NewsCarousel,NewsTagSelector, NewsBlockList},
+  components: { NewsCarousel, NewsTagSelector, NewsBlockList },
   methods: {
     handleTagSelected(tagId) {
       this.selectedTagId = tagId; /* 将选中的 tagId 传递给 NewsBlockList 组件作为 prop */
@@ -49,8 +49,8 @@ export default {
 
 <style>
 .HF_body {
-    margin: 30px auto 0 auto;
-    width: 85%;
+  margin: 30px auto 0 auto;
+  width: 85%;
 }
 
 .HF_title {
@@ -60,64 +60,63 @@ export default {
   padding-top: 0px;
 }
 
-.news_content{
-  width:auto;
-  height:auto;
+.news_content {
+  width: auto;
+  height: auto;
 }
 
-.left{
-  width:auto;
+.left {
+  width: auto;
   padding-left: 50px;
   padding-top: 20px;
 }
-.middle{
+
+.middle {
   width: auto;
 }
 
 .viewTitle {
-    margin-left: 1%;
-    background-image: linear-gradient(96.14deg,
-            rgba(0, 191, 168, 1) 0%,
-            #0093bf 100%);
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
+  padding-left: 40px;
+  background-image: linear-gradient(96.14deg,
+      rgba(0, 191, 168, 1) 0%,
+      #0093bf 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 
-    text-align: left;
-    font: 600 32px "Poppins", sans-serif;
+  text-align: left;
+  font: 600 32px "Poppins", sans-serif;
 }
 
 .tracking-in-expand {
-    -webkit-animation: tracking-in-expand 1s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
-    animation: tracking-in-expand 1s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
+  -webkit-animation: tracking-in-expand 1s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
+  animation: tracking-in-expand 1s cubic-bezier(0.215, 0.610, 0.355, 1.000) both;
 }
-
 @-webkit-keyframes tracking-in-expand {
-    0% {
-        letter-spacing: -0.5em;
-        opacity: 0;
-    }
+  0% {
+    letter-spacing: -0.5em;
+    opacity: 0;
+  }
 
-    40% {
-        opacity: 0.6;
-    }
+  40% {
+    opacity: 0.6;
+  }
 
-    100% {
-        opacity: 1;
-    }
+  100% {
+    opacity: 1;
+  }
 }
-
 @keyframes tracking-in-expand {
-    0% {
-        letter-spacing: -0.5em;
-        opacity: 0;
-    }
+  0% {
+    letter-spacing: -0.5em;
+    opacity: 0;
+  }
 
-    40% {
-        opacity: 0.6;
-    }
+  40% {
+    opacity: 0.6;
+  }
 
-    100% {
-        opacity: 1;
-    }
+  100% {
+    opacity: 1;
+  }
 }
 </style>
